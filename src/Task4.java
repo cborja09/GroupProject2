b57ba0ae79c1e0381c93facd5384a06930b5968d
cborja09@icloud.com <cborja09@icloud.com>
2023-05-26 22:28:44 -0700

    New Commit

--- src/Task4.java
+++ src/Task4.java
@@ -1,20 +1,34 @@
+import java.util.Arrays;
+
 public class Task4 {
     /*
     Check if Two Strings are Anagrams: Given two strings, determine if they are anagrams, meaning they contain the same
     characters in a different order. For example, "listen" and "silent" are anagrams.
      */
 
-    public static int countWords(String str) {
-        if (str.isEmpty()) {
-            return 0;
+    public static boolean areAnagrams(String str1, String str2) {
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            return false;
         }
 
-        String[] words = str.split("\\s+");
-        return words.length;
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
     }
     public static void main(String[] args) {
-        String input = "Hello, world!";
-        int wordCount = countWords(input);
-        System.out.println( wordCount);
+        String word = "listen";
+        String word1 = "silent";
+
+        if (areAnagrams(word, word1)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
     }
 }
