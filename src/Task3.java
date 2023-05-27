1990af7a509cdf41152143a9e3a0803c00477495
cborja09@icloud.com <cborja09@icloud.com>
2023-05-26 22:24:02 -0700

    New Commit

---
+++ src/Task3.java
@@ -0,0 +1,20 @@
+public class Task3 {
+    /*
+    Count the Number of Words in a String: Write a function to count the number of words in a given string.
+    Words are separated by spaces or punctuation. For example, the input "Hello, world!" should return 2.
+     */
+
+    public static int countWords(String str) {
+        if (str.isEmpty()) {
+            return 0;
+        }
+
+        String[] words = str.split("\\s+");
+        return words.length;
+    }
+    public static void main(String[] args) {
+        String input = "Hello, world!";
+        int wordCount = countWords(input);
+        System.out.println( wordCount);
+    }
+}
