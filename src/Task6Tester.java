a98a35de442972a9c2bbdce643e95c73cad91ab5
cborja09@icloud.com <cborja09@icloud.com>
2023-05-26 22:53:44 -0700

    New Commit

---
+++ src/Task6Tester.java
@@ -0,0 +1,13 @@
+public class Task6Tester {
+
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(5.0);
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Circle Perimeter: " + circle.calculatePerimeter());
+
+        Square square = new Square(7.0);
+        System.out.println("Square Area: " + square.calculateArea());
+        System.out.println("Square Perimeter: " + square.calculatePerimeter());
+    }
+}
